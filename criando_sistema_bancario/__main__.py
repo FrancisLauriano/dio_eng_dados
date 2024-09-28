75d13d73d5593122c8c42e68e17c7a8c31a02d19
FrancisLauriano <francislauriano@gmail.com>
2024-09-27 23:16:53 -0300

    finalizado

---
+++ criando_sistema_bancario/__main__.py
@@ -0,0 +1,56 @@
+#__main__.py:
+from time import sleep
+from utils import limpar_terminal, pausar_execucao
+from depositar import depositar
+from sacar import sacar
+from exibir_extrato import exibir_extrato
+
+
+def sistema_bancario():
+    saldo = 0
+    limite_saque = 500
+    extrato = []
+    saques_realizados = 0
+    LIMITE_SAQUES_DIARIOS = 3
+
+    while True:
+        limpar_terminal()
+        print(f'\n' + ' MENU '.center(20,'#'))
+        print(f'''[1] Depositar
+[2] Sacar
+[3] Extrato
+[0] Sair''')
+        print(f'\n' + '#' * 20)
+
+        try:
+            opcao = int(input('OPÇÃO: '))
+            limpar_terminal()
+
+            if opcao == 1:
+                saldo, extrato = depositar(saldo, extrato)
+            elif opcao == 2:
+                saldo, extrato, saques_realizados = sacar(saldo, extrato, saques_realizados, limite_saque, LIMITE_SAQUES_DIARIOS)
+            elif opcao == 3:
+                exibir_extrato(saldo, extrato)
+            elif opcao == 0:
+                limpar_terminal()
+                print(f'Encerrando o sistema...')
+                for i in range(5, 0, -1):
+                    print('|' * i)
+                    sleep(0.1)
+                print(f'FINALIZADO')
+                break
+            else:
+                print(f'Opção Inválida. Tente novamente...')
+                pausar_execucao()
+
+        except ValueError:
+            print(f'Opção inválida. Tente novamente.')
+            pausar_execucao()
+
+
+if __name__ == '__main__':
+    sistema_bancario()
+
+
+
---
+++ criando_sistema_bancario/utils.py
@@ -0,0 +1,12 @@
+# utils.py:
+from time import sleep
+import os
+
+def limpar_terminal():
+    sleep(0.5)
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def pausar_execucao():
+    input('\nPressione ENTER para continuar... ')        
+
+
