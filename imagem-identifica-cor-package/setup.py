6962204233b6405bcff9a9e19a2019ace504a3b4
FrancisLauriano <francislauriano@gmail.com>
2024-10-11 04:27:16 -0300

    ok

--- imagem-identifica-cor-package/setup.py
+++ imagem-identifica-cor-package/setup.py
@@ -9,8 +9,8 @@ with open("requirements.txt") as f:
 setup(
     name='identificador-img-color',
     version='0.1.0',
-    author="my_name",
-    author_email="my_email",
+    author="Francis Lauriano",
+    author_email="francislauriano@gmail.com",
     description='Pacote para identificar se uma imagem é colorida ou em preto e branco.',
     long_description=page_description,
     long_description_content_type="text/markdown",
